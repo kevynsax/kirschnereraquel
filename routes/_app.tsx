490d570fb5ef88d6756cb7780dc9d717241d1820
C1317023 Kevyn Pinheiro Klava <c1317023@interno.bb.com.br>
2024-11-07 15:33:51 -0300

    Menu, banner, capa

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,15 +2,20 @@ import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
     <html>
-      <head>
+    <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Kirschner e Raquel</title>
         <link rel="stylesheet" href="/styles.css" />
-      </head>
-      <body>
-        <Component />
-      </body>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Playwrite+BE+VLG:wght@100..400&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet" />
+    </head>
+    <body>
+    <Component />
+    </body>
     </html>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,86 @@
 import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
-  return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
-    </div>
-  );
+    const count = useSignal(3);
+
+    const targetDate = new Date("2024-12-14T17:00:00").getTime();
+    const days = Math.floor(
+        (targetDate - new Date().getTime()) / (1000 * 60 * 60 * 24),
+    );
+    const hours = Math.floor(
+        (targetDate - new Date().getTime()) / (1000 * 60 * 60) % 24,
+    );
+    const minutes = Math.floor(
+        (targetDate - new Date().getTime()) / (1000 * 60) % 60,
+    );
+    const seconds = Math.floor((targetDate - new Date().getTime()) / 1000 % 60);
+
+    const timings = [{
+        name: "dias",
+        value: days,
+    }, {
+        name: "horas",
+        value: hours,
+    }, {
+        name: "minutos",
+        value: minutes,
+    }, {
+        name: "segundos",
+        value: seconds,
+    }];
+
+    return (
+        <>
+            <div class="app-bar">
+                <div class="logo">
+                    <span>K</span>
+                    <span>&</span>
+                    <span>R</span>
+                </div>
+
+                <div class="menu">
+                    <span>Lista de presentes</span>
+                    <span>Mural de recados</span>
+                    <span>Casamento</span>
+                </div>
+            </div>
+            <div class="banner">
+                <div class="date">14.12.24</div>
+
+                <span class="name">
+                    Raquel Vasconcelos <br /> e <br /> Kirschner Klava
+                </span>
+            </div>
+
+            <div class="cover">
+                <img src="/cover.jpg" />
+
+                <div class="timings">
+                    {timings.map((timing) => (
+                        <div class="timing">
+                            <div class="value">{timing.value}</div>
+                            <div class="name">{timing.name}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+        // <div class="px-4 py-8 mx-auto bg-[#86efac]">
+        //   <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        //     <img
+        //       class="my-6"
+        //       src="/logo.svg"
+        //       width="128"
+        //       height="128"
+        //       alt="the Fresh logo: a sliced lemon dripping with juice"
+        //     />
+        //     <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        //     <p class="my-4">
+        //       Try updating this message in the
+        //       <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+        //     </p>
+        //     <Counter count={count} />
+        //   </div>
+        // </div>
+    );
 }
