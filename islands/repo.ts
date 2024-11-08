e8638b41a802083b2a048635c4b1a8236b23a2f6
C1317023 Kevyn Pinheiro Klava <c1317023@interno.bb.com.br>
2024-11-08 15:29:08 -0300

    Try to fix localstorage

--- islands/repo.ts
+++ islands/repo.ts
@@ -1,19 +1,11 @@
 
 
 export const setLocally = <T>(key: string, value: T) => {
-    if(typeof window === 'undefined') {
-        return null;
-    }
-
-    localStorage.setItem(key, JSON.stringify(value));
+    localStorage?.setItem(key, JSON.stringify(value));
 }
 
 export const getLocally = <T>(key: string): T | null => {
-    if(typeof window === 'undefined') {
-        return null;
-    }
-
-    const item = localStorage.getItem(key);
+    const item = localStorage?.getItem(key);
 
     if(!item) {
         return null;
